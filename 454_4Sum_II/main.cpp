1f6d99b66148a07688e94b9e02b3737815a8ffca
Hamlet <hamlet96@mail.ru>
2018-04-30 05:46:02 +0300

    performance fix for 454

--- 454_4Sum_II/main.cpp
+++ 454_4Sum_II/main.cpp
@@ -1,23 +1,22 @@
 #include <vector>
 #include <map>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
 	int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-		map<int, int> sum;
-		int count = 0;
+		unordered_map<int, int> sum;
+		
 		for (int i = 0; i < A.size(); i++) {
-			for (int j = 0; j < B.size(); j++) {
-				if (sum.find(A[i] + B[j]) != sum.end()) {
+			for (int j = i; j < B.size(); j++) {
+				if(sum.emplace(pair<int, int>(A[i] + B[j], 1)).second==false){
 					sum[A[i] + B[j]]++;
-				}
-				else {
-					sum.insert(pair<int,int>(A[i] + B[j], 1));
 				}
 			}
 		}
+		int count = 0;
 		for (int i = 0; i < C.size(); i++) {
 			for (int j = 0; j < D.size(); j++) {
 				if (sum[-(C[i] + D[j])] >0) {
